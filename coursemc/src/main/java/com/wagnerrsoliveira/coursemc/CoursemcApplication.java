2ae43af7ce881ce4f2bcd7b3f17438d754244e23
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-08-31 09:38:26 -0300

    coursemc: created project

---
+++ coursemc/src/main/java/com/wagnerrsoliveira/coursemc/CoursemcApplication.java
@@ -0,0 +1,13 @@
+package com.wagnerrsoliveira.coursemc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursemcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursemcApplication.class, args);
+	}
+
+}
---
+++ coursemc/src/test/java/com/wagnerrsoliveira/coursemc/CoursemcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagnerrsoliveira.coursemc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursemcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
