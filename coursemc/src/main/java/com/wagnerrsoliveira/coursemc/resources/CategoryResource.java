2bc21f0e28b6ab9b93a98dba4b7c368abb14047f
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-09-01 06:45:13 -0300

    coursemc: testing a first domain class - Category

--- coursemc/src/main/java/com/wagnerrsoliveira/coursemc/resources/CategoryResource.java
+++ coursemc/src/main/java/com/wagnerrsoliveira/coursemc/resources/CategoryResource.java
@@ -1,15 +1,27 @@
 package com.wagnerrsoliveira.coursemc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.wagnerrsoliveira.coursemc.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "Rest is working!";
+	public List<Category> list() {
+		Category cat1 = new Category(1, "Computing");
+		Category cat2 = new Category(1, "Office");
+		
+		List<Category> categories = new ArrayList<>();
+		categories.add(cat1);
+		categories.add(cat2);
+		
+		return categories;
 	}
 }
