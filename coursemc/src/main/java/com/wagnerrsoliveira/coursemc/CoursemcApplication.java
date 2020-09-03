3c18bf1038f8c91017ad81b1a1649ba1513bdbd1
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-09-03 06:23:14 -0300

    coursemc: created instantiation operantion

--- coursemc/src/main/java/com/wagnerrsoliveira/coursemc/CoursemcApplication.java
+++ coursemc/src/main/java/com/wagnerrsoliveira/coursemc/CoursemcApplication.java
@@ -1,13 +1,34 @@
 package com.wagnerrsoliveira.coursemc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CoursemcApplication {
+import com.wagnerrsoliveira.coursemc.domain.Category;
+import com.wagnerrsoliveira.coursemc.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class CoursemcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CoursemcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category category1 = new Category(null, "Computing");
+		Category category2 = new Category(null, "Office");
+		
+		categoryRepository.saveAll(Arrays.asList(category1,category2));
+		
+		
 	}
 
 }
